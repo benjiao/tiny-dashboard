0659b9c1a9ef8295db780dff325c2e9fd5c02514
“Benjie <hi@benjie.me>
2016-10-23 04:42:55 +0800

    Hello Git

---
+++ README.md
@@ -0,0 +1,5 @@
+# TinyDashboard
+An Arduino-powered dock that displays info via USB 
+
+![](images/IMG_2132.resized.JPG)
+```
---
+++ tiny_dashboard/tiny_dashboard.ino
@@ -0,0 +1,39 @@
+
+#include <LiquidCrystal.h>
+
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
+String line1 = "";
+String line2 = "";
+
+void printSerialLine(int line, String content){
+  // Clear line
+  lcd.setCursor(0, line);
+  lcd.print("                ");
+
+  // Display contents
+  lcd.setCursor(0, line);
+  lcd.print(content);
+}
+
+void setup(){
+  Serial.begin(9600);
+  
+  // LCD Setup
+  lcd.begin(16, 2);
+  printSerialLine(0, "Waiting for");
+  printSerialLine(1, "connection...");
+}
+
+void loop(){
+
+  while(Serial.available()==0);
+  
+  // Discard everything until this marker
+  Serial.readStringUntil('0c039ba7de191f928e8d2b1b445ac609');
+
+  line1 = Serial.readStringUntil('\n');
+  line2 = Serial.readStringUntil('\n');
+  printSerialLine(0, line1);
+  printSerialLine(1, line2);
+}
